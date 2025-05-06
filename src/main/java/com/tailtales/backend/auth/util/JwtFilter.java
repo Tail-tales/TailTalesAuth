ccb5b5570efc1dabe553a106e41ee0f40cb94260
eggzuxi <eggzuxi99@gmail.com>
2025-05-06 23:05:11 +0900

    :sparkles: feat: 기본적인 JWT 인증 흐름 구현

---
+++ src/main/java/com/tailtales/backend/auth/util/JwtFilter.java
@@ -0,0 +1,52 @@
+package com.tailtales.backend.auth.util;
+
+import com.tailtales.backend.auth.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+            final String jwtToken = authHeader.substring(7); // "Bearer " 제외
+
+            if (jwtUtil.validateAccessToken(jwtToken)) {
+                final String id = jwtUtil.getSubjectFromAccessToken(jwtToken);
+
+                if (id != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = this.customUserDetailsService.loadUserByUsername(id);
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+}
