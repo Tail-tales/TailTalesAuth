2ec2880b0879bf68b569370fdcc313c5daf6dcba
eggzuxi <eggzuxi99@gmail.com>
2025-05-19 12:24:16 +0900

    :fire: delete: 스프링 시큐리티 필터체인에서 소셜로그인 제거 임시 조치

--- src/main/java/com/tailtales/backend/config/SecurityConfig.java
+++ src/main/java/com/tailtales/backend/config/SecurityConfig.java
@@ -1,11 +1,7 @@
 package com.tailtales.backend.config;
 
-import com.tailtales.backend.auth.service.CustomOAuth2UserService;
 import com.tailtales.backend.auth.service.CustomUserDetailsService;
 import com.tailtales.backend.auth.util.JwtFilter;
-import com.tailtales.backend.auth.util.JwtUtil;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +10,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsUtils;
@@ -26,9 +21,6 @@ import static org.springframework.http.HttpMethod.POST;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private static final String COOKIE_NAME = "refreshToken";
-
-    private final JwtUtil jwtUtil;
     private final JwtFilter jwtFilter;
     private final CustomUserDetailsService customUserDetailsService;
     private final PasswordEncoder passwordEncoder;
@@ -42,17 +34,8 @@ public class SecurityConfig {
         auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder);
     }
 
-    private void addRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
-        Cookie refreshTokenCookie = new Cookie(COOKIE_NAME, refreshToken);
-        refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setMaxAge((int) (jwtUtil.getRefreshTokenValiditySeconds() / 1000));
-        refreshTokenCookie.setPath("/");
-        refreshTokenCookie.setSecure(false);
-        response.addCookie(refreshTokenCookie);
-    }
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomOAuth2UserService customOAuth2UserService) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf((csrf) -> csrf.disable()) // CSRF 비활성화
                 .authorizeHttpRequests((auth) -> auth
@@ -69,37 +52,6 @@ public class SecurityConfig {
                         sessionManagement.sessionCreationPolicy(org.springframework.security.config.http.SessionCreationPolicy.STATELESS) // 세션 사용 안 함
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                        .successHandler((request, response, authentication) -> {
-                            OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-                            String accessToken = oAuth2User.getAttribute("accessToken");
-                            String refreshToken = oAuth2User.getAttribute("refreshToken");
-                            String nickname = oAuth2User.getAttribute("nickname");
-                            String email = oAuth2User.getAttribute("email");
-
-                            // Access Token 쿠키에 저장
-                            Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
-                            accessTokenCookie.setHttpOnly(true);
-                            accessTokenCookie.setPath("/");
-                            accessTokenCookie.setSecure(false);
-                            response.addCookie(accessTokenCookie);
-
-                            // Refresh Token 쿠키에 저장
-                            addRefreshTokenCookie(response, refreshToken);
-
-                            // 닉네임 쿠키에 저장 (선택 사항)
-                            Cookie nicknameCookie = new Cookie("nickname", nickname);
-                            nicknameCookie.setPath("/");
-                            response.addCookie(nicknameCookie);
-
-                            // 이메일 쿠키에 저장 (선택 사항)
-                            Cookie emailCookie = new Cookie("email", email);
-                            emailCookie.setPath("/");
-                            response.addCookie(emailCookie);
-
-                            // 리다이렉트 (프론트 페이지 생성 시 변경 필수!!!!!!)
-                            response.sendRedirect("http://localhost:5173");
-                        }))
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable());
         return http.build();
