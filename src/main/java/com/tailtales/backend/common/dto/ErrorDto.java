6a5f13f807e9b9245a41416e97cbf15159a869af
eggzuxi <eggzuxi99@gmail.com>
2025-05-27 19:24:43 +0900

    :bug: fix: ErrorDto 파싱 에러 해결

--- src/main/java/com/tailtales/backend/common/dto/ErrorDto.java
+++ src/main/java/com/tailtales/backend/common/dto/ErrorDto.java
@@ -3,10 +3,12 @@ package com.tailtales.backend.common.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorDto {
 
     private String code;
